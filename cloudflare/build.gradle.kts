9c91b9b8f92aca2626b69416a6bb94ef0bff476e
zhaolongqing <2232792471@qq.com>
2023-10-22 15:54:49 +0800

    更新工程

--- cloudflare/build.gradle.kts
+++ cloudflare/build.gradle.kts
@@ -11,7 +11,7 @@ version = "1.0-SNAPSHOT"
 application {
     mainClass.set("com.ddns.cloudflare.Main")
     applicationName = "ddns"
-    mainModule.set("ddns")
+//    mainModule.set("ddns")
 }
 
 dependencies {
